3206c4d79518f6b9e07795f124dad924f6eb1442
Jhonny Freitas Dutra <dutrajy@gmail.com>
2024-09-29 15:26:38 -0300

    :hotfix: arrumando case do nome da tabela na query

--- dataViz/src/main/java/fatec/bytelabss/dataViz/repositorys/FatoContratacoesRepository.java
+++ dataViz/src/main/java/fatec/bytelabss/dataViz/repositorys/FatoContratacoesRepository.java
@@ -18,7 +18,7 @@ import fatec.bytelabss.dataViz.models.FatoContratacoes;
 public interface FatoContratacoesRepository extends JpaRepository<FatoContratacoes, Integer>{
 
 
-  	@Query(nativeQuery = true, value =  "SELECT a.processo_seletivo, sum(a.tempo_medio) / count(a.processo_seletivo) tempo_medio, b.nome FROM FATO_CONTRATACOES a INNER JOIN DIM_PROCESSO_SELETIVO b ON b.id_processo_seletivo = a.processo_seletivo WHERE (b.inicio_processo_seletivo < :fim || :fim is null) and (b.fim_processo_seletivo > :inicio or b.fim_processo_seletivo is null)  GROUP BY a.processo_seletivo")
+  	@Query(nativeQuery = true, value =  "SELECT a.processo_seletivo, sum(a.tempo_medio) / count(a.processo_seletivo) tempo_medio, b.nome FROM fato_contratacoes a INNER JOIN dim_processo_seletivo b ON b.id_processo_seletivo = a.processo_seletivo WHERE (b.inicio_processo_seletivo < :fim || :fim is null) and (b.fim_processo_seletivo > :inicio or b.fim_processo_seletivo is null)  GROUP BY a.processo_seletivo")
     List<ProcessoSeletivoTempoMedioDto> RetornarTempoMedioProcessoSeletivo(@Param("inicio") LocalDateTime inicio, @Param("fim") Optional<LocalDateTime> fim);
 
 
