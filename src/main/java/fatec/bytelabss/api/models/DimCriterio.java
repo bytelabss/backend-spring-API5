506c991e94a7580529e9f458c293124841a47078
Antonio Zago <80283126+Antonio-Zago@users.noreply.github.com>
2024-10-07 21:27:36 -0300

    BYTE-65 feat: Criação das tabelas da estrutura de fato avaliações + criação das classes de mapeamento

---
+++ src/main/java/fatec/bytelabss/api/models/DimCriterio.java
@@ -0,0 +1,24 @@
+package fatec.bytelabss.api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "DIM_CRITERIO")
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class DimCriterio {
+
+	@Id
+    @Column(name = "id_criterio")
+    private Long idCriterio;
+
+    @Column(name = "nome")
+    private String nome;
+}
