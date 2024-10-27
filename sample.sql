e9e612b8b00dd4f0fc340788586481cd2dd8657f
Antonio Zago <80283126+Antonio-Zago@users.noreply.github.com>
2024-10-27 16:56:56 -0300

    fix: ajuste ci

--- sample.sql
+++ sample.sql
@@ -4,10 +4,6 @@
 -- ------------------------------------------------------
 -- Server version	8.0.37
 
-CREATE USER IF NOT EXISTS 'userapp'@'localhost' IDENTIFIED BY 'admin123';
-
-GRANT SELECT, INSERT, CREATE, UPDATE, DELETE ON dataviz_bytelabss.* TO 'userapp'@'localhost';
-
 USE dataviz_bytelabss;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
