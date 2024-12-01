914dac2428c00d2470565da37d3dc4b8f828fa07
Antonio Zago <80283126+Antonio-Zago@users.noreply.github.com>
2024-12-01 18:27:53 -0300

    fix: ajuste teste

--- src/test/java/fatec/bytelabss/api/testesIntegracao/ImportacaoDwCargaTeste1IntegrationTests.java
+++ src/test/java/fatec/bytelabss/api/testesIntegracao/ImportacaoDwCargaTeste1IntegrationTests.java
@@ -124,8 +124,8 @@ public class ImportacaoDwCargaTeste1IntegrationTests {
 	            assertEquals("Concluído", row1.getCell(2).getStringCellValue(), "Status do Processo Seletivo na linha 1 deve ser 'pendente'");
 	            assertEquals("Descrição do processo seletivo 1", row1.getCell(3).getStringCellValue(), "Descrição do Processo Seletivo na linha 1 deve ser 'Contratar novos estagiarios e aprendizes para o ano de 2024'");
 	            assertEquals("Antonio", row1.getCell(4).getStringCellValue(), "Criado Por na linha 1 deve ser 'Rodrigo'");
-	            assertEquals("2024-01-01T00:00", row1.getCell(5).getStringCellValue(), "Data de Início do Processo Seletivo na linha 1 deve ser '2024-01-01'");
-	            assertEquals("2024-12-30T00:00", row1.getCell(6).getStringCellValue(), "Data de Fim do Processo Seletivo na linha 1 deve ser '2024-12-30'");
+	            assertEquals("2023-12-01T00:00", row1.getCell(5).getStringCellValue(), "Data de Início do Processo Seletivo na linha 1 deve ser '2024-01-01'");
+	            assertEquals("2024-01-30T00:00", row1.getCell(6).getStringCellValue(), "Data de Fim do Processo Seletivo na linha 1 deve ser '2024-12-30'");
 
 	            Row row2 = sheet.getRow(2);
 	            assertEquals(2L, row2.getCell(0).getNumericCellValue(), "ID Processo Seletivo na linha 2 deve ser 2");
