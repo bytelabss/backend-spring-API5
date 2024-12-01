a42b1d6f11d599fe56d461cce1bd2f3688f3a2da
Antonio Zago <80283126+Antonio-Zago@users.noreply.github.com>
2024-12-01 18:56:01 -0300

    fix: ajuste

--- src/test/java/fatec/bytelabss/api/testesIntegracao/ImportacaoDwCargaTeste1IntegrationTests.java
+++ src/test/java/fatec/bytelabss/api/testesIntegracao/ImportacaoDwCargaTeste1IntegrationTests.java
@@ -43,7 +43,7 @@ import fatec.bytelabss.api.services.excel.DimParticipanteRHExcelService;
 import fatec.bytelabss.api.services.excel.DimProcessoSeletivoExcelService;
 
 @SpringBootTest
-//@ActiveProfiles("test")
+@ActiveProfiles("test")
 public class ImportacaoDwCargaTeste1IntegrationTests {
 	 
 	 @Autowired
